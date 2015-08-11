f6ce88fc09ee29a9229bad7d0402ca12f9635926
Krystin Stutesman <krystin09@gmail.com>
2015-08-11 18:02:27 -0400

    Moved Login to XAML

---
+++ SwarmPrototype/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SwarmPrototype
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+
+			NavigationPage.SetHasNavigationBar (this, false);
+
+			buttonLogin.Clicked += async (object sender, EventArgs e) => {
+				await Navigation.PushAsync (new ActivityFeedPage ());
+			};
+		}
+	}
+}
+
